58781450cc27fbae1d2dced4bdc3f836e9ced433
marvelmd@gmail.com <marvelmd@gmail.com>
2018-06-26 16:34:09 +0300

    Publisher added

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorsRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -12,10 +14,12 @@ import org.springframework.stereotype.Component;
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private AuthorsRepository authorsRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorsRepository authorsRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorsRepository authorsRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorsRepository = authorsRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -24,16 +28,20 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     private void initData() {
+        Publisher publisher = new Publisher();
+        publisher.setName("foo");
+        publisherRepository.save(publisher);
+
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Development", "123", "helper Garis");
+        Book ddd = new Book("Domain Driven Development", "123", publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthor().add(eric);
         authorsRepository.save(eric);
         bookRepository.save(ddd);
         //Rode
         Author rode = new Author("Rode", "Bush");
-        Book bbb = new Book("Domain Driven Testing", "567", "helper Gregory");
+        Book bbb = new Book("Domain Driven Testing", "567", publisher);
         rode.getBooks().add(bbb);
         bbb.getAuthor().add(rode);
         authorsRepository.save(rode);
